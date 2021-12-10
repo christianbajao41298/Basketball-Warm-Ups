43838590a80dc39384cf2b4452d0200c09f666fc
christianbajao <christian.bajao@cvsu.edu.ph>
2021-12-10 13:34:06 +0800

    Basketball Warm Ups / 12/10/2021

---
+++ app/src/main/java/com/example/basketballwarmups/RestArmStretch.java
@@ -0,0 +1,14 @@
+package com.example.basketballwarmups;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestArmStretch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arm_stretch_rest);
+    }
+}
---
+++ app/src/main/java/com/example/basketballwarmups/RestSideSlide.java
@@ -0,0 +1,14 @@
+package com.example.basketballwarmups;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RestSideSlide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arm_circles_rest);
+    }
+}

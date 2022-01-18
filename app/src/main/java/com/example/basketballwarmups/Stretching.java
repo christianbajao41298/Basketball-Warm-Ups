99af42f1b9505c3e626eecac28ec201792c7c01b
christianbajao <christian.bajao@cvsu.edu.ph>
2022-01-19 00:43:07 +0800

    Basketball Warm Ups / 1/19/2021

--- app/src/main/java/com/example/basketballwarmups/Stretching.java
+++ app/src/main/java/com/example/basketballwarmups/Stretching.java
@@ -2,17 +2,16 @@ package com.example.basketballwarmups;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 public class Stretching extends AppCompatActivity {
-    Button myButton;
+        ImageButton myImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +21,42 @@ public class Stretching extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_stretching);
 
-        myButton = findViewById(R.id.button4);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Stretching.this, ArmCircles.class);
-                startActivity(intent);
-            }
+        myImageButton = findViewById(R.id.armStretchLeftButtonIcon3);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Stretching.this,ArmStretch.class);
+            startActivity(intent);
         });
+        myImageButton = findViewById(R.id.armStretchRightButtonIcon3);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Stretching.this,ArmStretch2.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.kneeHugsLeftButtonIcon3);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Stretching.this,KneeHugsLeft.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.kneeHugsRightButtonIcon3);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Stretching.this,KneeHugsRight.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.kneeStretchLeftButtonIcon3);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Stretching.this,KneeStretchLeft.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.kneeStretchRightButtonIcon3);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Stretching.this,KneeStretchRight.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.walkingLungeWithRotationButtonIcon3);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(Stretching.this,WalkingLungeWithRotation.class);
+            startActivity(intent);
+        });
+
+
     }
 }

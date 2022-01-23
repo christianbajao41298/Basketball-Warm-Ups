702e2e1fe66b696c45a5bee1739314af020749e1
christianbajao <christian.bajao@cvsu.edu.ph>
2022-01-23 13:34:28 +0800

    Basketball Warm Ups / 1/19/2021

---
+++ app/src/main/java/com/example/basketballwarmups/BallDribble.java
@@ -0,0 +1,89 @@
+package com.example.basketballwarmups;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
+import java.util.Objects;
+
+public class BallDribble extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 45000;
+    private TextView mTextViewCountDown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_ball_dribble);
+
+        mTextViewCountDown = findViewById(R.id.countdown_text);
+        mButtonStartPause = findViewById(R.id.startButton);
+        mButtonReset = findViewById(R.id.resetButton);
+        mButtonStartPause.setOnClickListener(v -> {
+            if (mTimerRunning) {
+                pauseTimer();
+            } else {
+                startTimer();
+            }
+        });
+        mButtonReset.setOnClickListener(v -> resetTimer());
+        updateCountDownText();
+    }
+
+    private void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    private void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Continue");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    private void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setText("Start");
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        mTextViewCountDown.setText(timeLeftFormatted);
+    }
+}
--- app/src/main/java/com/example/basketballwarmups/WithBall.java
+++ app/src/main/java/com/example/basketballwarmups/WithBall.java
@@ -1,14 +1,17 @@
 package com.example.basketballwarmups;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Objects;
 
 public class WithBall extends AppCompatActivity {
+    ImageButton myImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +20,36 @@ public class WithBall extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_with_ball);
+
+        myImageButton = findViewById(R.id.ballDribbleButtonIcon);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WithBall.this,BallDribble.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.ballTapButtonIcon);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WithBall.this,BallTap.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.waistWrapButtonIcon);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WithBall.this,WaistWrap.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.sideLungeWithTouchButtonIcon);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WithBall.this,SideLungeWithTouch.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.backLungeWithPassBackButtonIcon);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WithBall.this,BackLungeWithPassBack.class);
+            startActivity(intent);
+        });
+        myImageButton = findViewById(R.id.singleLegRollButtonIcon);
+        myImageButton.setOnClickListener(v -> {
+            Intent intent = new Intent(WithBall.this,SingleLegRoll.class);
+            startActivity(intent);
+        });
     }
 }

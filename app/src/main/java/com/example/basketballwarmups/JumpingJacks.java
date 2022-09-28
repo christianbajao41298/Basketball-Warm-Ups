35f2fd080ff32b7b4efc3652c3581fdca82e8aef
christianbajao <christian.bajao@cvsu.edu.ph>
2022-09-28 22:18:24 +0800

    September 28, 2022 / 10pm

--- app/src/main/java/com/example/basketballwarmups/JumpingJacks.java
+++ app/src/main/java/com/example/basketballwarmups/JumpingJacks.java
@@ -1,43 +1,89 @@
 package com.example.basketballwarmups;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.support.v4.app.INotificationSideChannel;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Locale;
 
 public class JumpingJacks extends AppCompatActivity {
-    private static final long START_TIME_IN_MILLIS = 45000;
+
+    ImageView img;
+    ImageButton myImageHomeButton;
+    DBHelper DB = new DBHelper(this);
+    private static long START_TIME_IN_MILLIS;
     private TextView mTextViewCountDown;
     private Button mButtonStartPause;
     private Button mButtonReset;
     private CountDownTimer mCountDownTimer;
     private boolean mTimerRunning;
     private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    VideoView videoView;
+    private INotificationSideChannel.Default someCountDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jumping_jacks);
 
+        //get timer data
+        int data = DB.getTimerData();
+        START_TIME_IN_MILLIS = data*1000;
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+
+
+        VideoView videoView =findViewById(R.id.videoView2);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.kneehugs);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+        mediaController.setVisibility(View.INVISIBLE);
+
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.setLooping(true);
+            }
+        });
+
         mTextViewCountDown = findViewById(R.id.countdown_text);
         mButtonStartPause = findViewById(R.id.startButton);
         mButtonReset = findViewById(R.id.resetButton);
-        mButtonStartPause.setOnClickListener(v -> {
-            if (mTimerRunning) {
-                pauseTimer();
-            } else {
-                startTimer();
+        startTimer();
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (mTimerRunning) {
+                    pauseTimer();
+                    videoView.pause();
+                } else {
+                    startTimer();
+                    videoView.start();
+                }
+
             }
         });
-        mButtonReset.setOnClickListener(v -> resetTimer());
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { resetTimer();
+            }
+        });
         updateCountDownText();
+//pogi
     }
-
     private void startTimer() {
         mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
             @Override
@@ -48,36 +94,68 @@ public class JumpingJacks extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                mTimerRunning = false;
-                mButtonStartPause.setText("Start");
-                mButtonStartPause.setVisibility(View.INVISIBLE);
-                mButtonReset.setVisibility(View.VISIBLE);
+                startActivity(new Intent(getApplicationContext(),RestKneeStretchLeft.class));
+            }
+        }.start();
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+
+    }
+    private void pauseTimer(){
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Continue");
+        mButtonReset.setVisibility(View.VISIBLE );
+
+    }
+    private void resetTimer(){
+        //get timer data
+        int data = DB.getTimerData();
+        START_TIME_IN_MILLIS = data*1000;
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setText("Pause");
+        mButtonStartPause.setVisibility(View.VISIBLE);
+
+        VideoView videoView =findViewById(R.id.videoView2);
+        videoView.setVideoPath("android.resource://"+getPackageName()+"/"+R.raw.kneehugs);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.start();
+        mediaController.setVisibility(View.INVISIBLE);
+        videoView.setMediaController(mediaController);
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(),RestKneeStretchLeft.class));
+                finish();
             }
         }.start();
         mTimerRunning = true;
         mButtonStartPause.setText("Pause");
         mButtonReset.setVisibility(View.INVISIBLE);
     }
-
-    private void pauseTimer() {
-        mCountDownTimer.cancel();
-        mTimerRunning = false;
-        mButtonStartPause.setText("Continue");
-        mButtonReset.setVisibility(View.VISIBLE);
-    }
-
-    private void resetTimer() {
-        mTimeLeftInMillis = START_TIME_IN_MILLIS;
-        updateCountDownText();
-        mButtonReset.setVisibility(View.INVISIBLE);
-        mButtonStartPause.setText("Start");
-        mButtonStartPause.setVisibility(View.VISIBLE);
-    }
-
-    private void updateCountDownText() {
+    private void updateCountDownText(){
         int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
         int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
         String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
         mTextViewCountDown.setText(timeLeftFormatted);
+    }
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        mCountDownTimer.cancel();
+        startActivity(new Intent(JumpingJacks.this, WholeBodyDynamic3.class));
+        finish();
     }
 }
